412b47168ce99073c885791ab526efdc13089495
hugo-wemer <wemer.eng@gmail.com>
2025-09-17 11:18:47 -0300

    feat: animation for setting load

--- src/renderer/components/settings-container.tsx
+++ src/renderer/components/settings-container.tsx
@@ -4,6 +4,7 @@ import { Loader2, Cog } from 'lucide-react'
 import type { RegisterReadingsResponse } from 'shared/types'
 import { SettinsTree } from './ui/settings-tree'
 import { SettingForm } from './ui/setting-form'
+import { motion } from 'motion/react'
 
 export function SettingsContainer({
   isFetchingBlocks,
@@ -22,8 +23,13 @@ export function SettingsContainer({
       <div className="flex flex-1  gap-8">
         <div className="w-1/2">
           {isFetchingBlocks ? (
-            <div className="bg-card h-[calc(100vh-95px)] rounded border border-muted-foreground flex items-center justify-center">
-              <Loader2 className="animate-spin text-muted-foreground" />
+            <div className="h-[calc(100vh-85px)]">
+              <motion.div
+                data-progress={isFetchingBlocks}
+                className="bg-card rounded-xl h-10 flex items-center justify-center border border-transparent animate-border data-[progress=true]:[background:linear-gradient(45deg,#09090B,theme(colors.zinc.900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.zinc.700/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.zinc.600/.48))_border-box]"
+              >
+                <Loader2 className="animate-spin text-muted-foreground" />
+              </motion.div>
             </div>
           ) : (
             <SettinsTree registers={registers} setSelectedSetting={setSelectedSetting} />
